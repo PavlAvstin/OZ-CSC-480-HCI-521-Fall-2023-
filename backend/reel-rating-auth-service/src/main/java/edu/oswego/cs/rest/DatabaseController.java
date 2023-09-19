024ee75bd03b1a4f26d5d70f57903672340652c6
Mahella Soavelo <msoavelo@oswego.edu>
2023-09-18 20:57:01 -0400

    Added create user function to database controller.

--- backend/reel-rating-auth-service/src/main/java/edu/oswego/cs/rest/DatabaseController.java
+++ backend/reel-rating-auth-service/src/main/java/edu/oswego/cs/rest/DatabaseController.java
@@ -3,6 +3,10 @@ package edu.oswego.cs.rest;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class DatabaseController {
   
@@ -12,5 +16,18 @@ public class DatabaseController {
   public MongoDatabase getUserCredentialsDatabase() {
     MongoClient mongoClient = MongoClients.create(mongoURL);
     return mongoClient.getDatabase(mongoDatabaseName);
+  }
+
+  //This asssumes that the databae has the collection already.
+  public void createUser(String username, String password, String saltHash, String sessionId, String dateTime) {
+      var database = getUserCredentialsDatabase();
+      var users = database.getCollection("users");
+      var userDocument = new Document();
+      userDocument.put("username", username);
+      userDocument.put("password", password);
+      userDocument.put("saltHash", saltHash);
+      userDocument.put("sessionId", sessionId);
+      userDocument.put("dateTime", dateTime);
+      users.insertOne(userDocument);
   }
 }
