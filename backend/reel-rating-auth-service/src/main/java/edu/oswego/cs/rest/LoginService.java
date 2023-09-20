ee079689888ed723d8150bb9d9faf9062e738280
DeveloperDave17 <DavidHennigan17@gmail.com>
2023-09-19 20:53:51 -0400

    create registerUser

--- backend/reel-rating-auth-service/src/main/java/edu/oswego/cs/rest/LoginService.java
+++ backend/reel-rating-auth-service/src/main/java/edu/oswego/cs/rest/LoginService.java
@@ -1,5 +1,8 @@
 package edu.oswego.cs.rest;
 
+import java.security.NoSuchAlgorithmException;
+import java.time.LocalDateTime;
+
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.ws.rs.Consumes;
@@ -18,14 +21,32 @@ public class LoginService {
   @POST
   @Consumes(MediaType.APPLICATION_JSON)
   @Path("/login")
-  public Response login(@Context HttpServletRequest request, @QueryParam("username") String username, @QueryParam("password") String password) {
+  public Response login(@Context HttpServletRequest request, @QueryParam("username") String username, @QueryParam("password") String password) throws NoSuchAlgorithmException {
     DatabaseController db = new DatabaseController();
     if (db.checkIfUserExists(username)) {
-      String sessionId = request.getSession().getId();
-      String stateMessage = "logged in";
-      return Response.ok(stateMessage).build();
-    } else {
-      return Response.status(Status.UNAUTHORIZED).build();
+      if (SecurityUtils.validatePassword(password, db.getPassword(username))) {
+        String sessionId = request.getSession().getId();
+        db.setUserSessionId(username, sessionId);
+        String stateMessage = "logged in";
+        return Response.ok(stateMessage).build();
+      }
     }
+    return Response.status(Status.UNAUTHORIZED).build();
+  }
+
+  @POST
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Path("/register")
+  public Response registerUser(@Context HttpServletRequest request, @QueryParam("username") String username, @QueryParam("password") String password) throws NoSuchAlgorithmException {
+    DatabaseController db = new DatabaseController();
+    if (!db.checkIfUserExists(username)) {
+      String encryptedPassword = SecurityUtils.generatePassword(password);
+      String sessionId = request.getSession().getId();
+      String dateTime = LocalDateTime.now().toString();
+      db.createUser(username, encryptedPassword, sessionId, dateTime);
+      String stateMessage = "Registered";
+      return Response.ok(stateMessage).build();
+    }
+    return Response.status(Status.UNAUTHORIZED).build();
   }
 }
