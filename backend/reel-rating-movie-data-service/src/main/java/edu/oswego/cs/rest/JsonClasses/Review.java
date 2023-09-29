21759f9d5c3e759ad859f14294865a4aca91c0bc
DeveloperDave17 <DavidHennigan17@gmail.com>
2023-09-29 10:18:13 -0400

    create jsonclasses

---
+++ backend/reel-rating-movie-data-service/src/main/java/edu/oswego/cs/rest/JsonClasses/Review.java
@@ -0,0 +1,33 @@
+package edu.oswego.cs.rest.JsonClasses;
+
+public class Review {
+  private String movieTitle;
+  private String reviewTitle;
+  private String reviewDescription;
+
+  public Review() {}
+
+  public String getMovieTitle() {
+    return movieTitle;
+  }
+
+  public void setMovieTitle(String movieTitle) {
+    this.movieTitle = movieTitle;
+  }
+
+  public String getReviewTitle() {
+    return reviewTitle;
+  }
+
+  public void setReviewTitle(String reviewTitle) {
+    this.reviewTitle = reviewTitle;
+  }
+
+  public String getReviewDescription() {
+    return reviewDescription;
+  }
+
+  public void setReviewDescription(String reviewDescription) {
+    this.reviewDescription = reviewDescription;
+  }
+}
