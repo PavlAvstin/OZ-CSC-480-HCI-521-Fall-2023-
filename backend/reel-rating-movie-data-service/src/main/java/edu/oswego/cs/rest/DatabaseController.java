c59bd7b7be3ca7f2e11c49e66a90901c105f04e2
Keith Allen <kallen20@oswego.edu>
2023-10-03 18:17:25 -0400

    Start of Create CRUD ops
    createFlag(), createReview(), createActor(), createMovie() have all been started. Create movie needs to be finalized in relation to actors and writers. All are self contained and require no helper functions. More informatiive javaDocs will be added in the future

--- backend/reel-rating-movie-data-service/src/main/java/edu/oswego/cs/rest/DatabaseController.java
+++ backend/reel-rating-movie-data-service/src/main/java/edu/oswego/cs/rest/DatabaseController.java
@@ -43,4 +43,157 @@ public class DatabaseController {
     return getMovieDatabase().getCollection("reviews");
   }
 
+  /**
+   * Create CRUD operations
+   *
+   */
+
+
+  /**
+   *
+   *
+   * Users are not allowed to create a flag for a movie that does not already exist. If the movie does not exist
+   *
+   * @param flagName
+   * @param movieTitleToAdd
+   * @param movieId
+   */
+  public void createFlag(String flagName, String movieTitleToAdd, String movieId) {
+    // get the collections
+    MongoCollection<Document> flagCollection = getFlagCollection();
+    MongoCollection<Document> movieCollection = getMovieCollection();
+
+    // grab the two possible iterations of the flag that could exist
+    Document existsAndFlagged = flagCollection.find(Filters.eq("movieTitles", movieTitleToAdd)).first();
+    Document existingFlag = flagCollection.find(Filters.eq("flagName", flagName)).first();
+
+    // if the flag exists and the movie is already flagged
+    if (null != existsAndFlagged){ }
+
+    // if the flag exists and the movie is not flagged
+    else if (null != existingFlag) {
+      Document movie = movieCollection.find(Filters.eq("id", movieId)).first();
+      // if the movie exists
+      if(null != movie) {
+        // push the movieName to the flag list
+        Bson flagUpdateOperation = Updates.push("movieTitles", movieTitleToAdd);
+        flagCollection.updateOne(existingFlag, flagUpdateOperation);
+        // push the flagName to the movie list
+        Bson movieUpdateOperation = Updates.push("flagNames", flagName);
+        movieCollection.updateOne(movie, movieUpdateOperation);
+      }
+      // if the movie does not exist
+      else{ }
+    }
+    // if the flag does not exist
+    else {
+      Document movie = movieCollection.find(Filters.eq("id", movieId)).first();
+      // if the movie exists
+      if(null != movie) {
+        // create the flag and add to the collection
+        Document newFlag = new Document("flagName", flagName).append("movieTitles", movieTitleToAdd);
+        flagCollection.insertOne(newFlag);
+        // push the flagName to the movie list
+        Bson movieUpdateOperation = Updates.push("flagNames", flagName);
+        movieCollection.updateOne(movie, movieUpdateOperation);
+      }
+      // if the movie does not exist
+      else{ }
+    }
+  }
+
+  public void createRating(String ratingCategoryName){
+
+  }
+
+  /**
+   *
+   * @param movieTitle
+   * @param movieId
+   * @param reviewTitle
+   * @param reviewDescription
+   * @param userName
+   */
+  public void createReview(String movieTitle, String movieId, String reviewTitle, String reviewDescription, String userName){
+    // get collections
+    MongoCollection<Document> reviewCollection = getReviewCollection();
+    MongoCollection<Document> movieCollection = getMovieCollection();
+
+    // get the movie object to make sure it exists
+    Document movie = movieCollection.find(Filters.eq("id", movieId)).first();
+
+    // if the movie exists
+    if(null != movie) {
+      // create a new review
+      Document newReview = new Document("movieTitle", movieTitle).append("reviewTitle", reviewTitle)
+              .append("reviewDescription", reviewDescription).append("userName", userName);
+      reviewCollection.insertOne(newReview);
+    }
+    // if the movie does not exist
+    else{ }
+  }
+
+  /**
+   *
+   * @param actorName
+   * @param actorId
+   * @param dob
+   * @param movieTitle
+   * @param movieId
+   */
+  public void createActor(String actorName, String actorId, String dob, String movieTitle, String movieId){
+    // get collections
+    MongoCollection<Document> actorCollection = getActorCollection();
+    MongoCollection<Document> movieCollection = getMovieCollection();
+
+    // get the actor object to see if it exists
+    Document actor = actorCollection.find(Filters.eq("id", actorId)).first();
+
+    // if the actor exists
+    if(null != actor) { }
+
+    // if the actor does not exist
+    else{
+      // get the movie object to make sure it exists
+      Document movie = movieCollection.find(Filters.eq("id", movieId)).first();
+      // if the movie exists
+      if(null != movie) {
+        // create a new actor
+        Document newReview = new Document("id", actorId).append("name", actorName)
+                .append("dob", dob).append("movies", movieTitle);
+        actorCollection.insertOne(newReview);
+
+        // add actor to movie cast
+        Bson movieUpdateOperation = Updates.push("principalCast", actorName);
+        movieCollection.updateOne(movie, movieUpdateOperation);
+      }
+      // if the movie does not exist
+      else{ }
+    }
+  }
+
+  public void createMovie(String movieTitle, String movieId, String director, String principalCast, String releaseDate,
+                          String runtime, String writers, String plotSummary){
+    // get collections
+    MongoCollection<Document> movieCollection = getMovieCollection();
+
+    // get the movie object to see if it exists
+    Document movie = movieCollection.find(Filters.eq("id", movieId)).first();
+
+    // if the movie exists
+    if (null != movie) { }
+
+    // if the movie does not exist
+    else{
+      // create a new movie and add it to the movie collection
+      Document newMovie = new Document("id", movieId).append("Title", movieTitle).append("Director", director)
+              .append("releaseDate", releaseDate).append("Runtime", runtime).append("plotSummary", plotSummary);
+      movieCollection.insertOne(newMovie);
+    }
+  }
+
+  /**
+   * Helper functions
+   */
+
 }
