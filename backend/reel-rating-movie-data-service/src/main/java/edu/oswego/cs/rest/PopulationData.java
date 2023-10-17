26418bf911ad63542e869590eacb74c17fa91708
DeveloperDave17 <DavidHennigan17@gmail.com>
2023-10-17 19:27:30 -0400

    clean up data

--- backend/reel-rating-movie-data-service/src/main/java/edu/oswego/cs/rest/PopulationData.java
+++ backend/reel-rating-movie-data-service/src/main/java/edu/oswego/cs/rest/PopulationData.java
@@ -9,10 +9,10 @@ public class PopulationData {
         db.createMovie("The Sound of Music","Robert Wise", "1965", "2h. 52min.", "Georg Hurdalek, Howard Lindsay, Russel Crouse", "A nun ends up getting kicked out of the monastery after falling in love with a man and he has like 5 kids. Tragic, but he’s rich so it’s not so bad. He’s divorced. It’s a musical.");
 
         var movie1 = db.getMovieWithTitle("The Sound of Music");
-        db.createTag("Romance", movie1.get().getId());
-        db.createTag("Drama", movie1.get().getId());
-        db.createTag("Family", movie1.get().getId());
-        db.createTag("Biography", movie1.get().getId());
+        db.createTag("Romance", movie1.get().getId(), "David", "public");
+        db.createTag("Drama", movie1.get().getId(), "David", "public");
+        db.createTag("Family", movie1.get().getId(), "David", "public");
+        db.createTag("Biography", movie1.get().getId(), "David", "public");
 
         db.createRating("Blasphemy Against Vows", "5", "5", "David", movie1.get().getId(), "public");
         db.createRating("Is it worth it?", "10", "10", "David", movie1.get().getId(), "public");
@@ -23,9 +23,9 @@ public class PopulationData {
                 "An ungrateful middle-aged man gets the chance to be 17 again because he had a fixation on his high-school glory days and gets a chance to be “17 again” by a magical janitor. His best friend was cool. His wife rightfully wants to divorce him after putting up with his ridiculousness for 20 years. But then he learns to appreciate what he has or something and she decides not to divorce him (unfortunately).");
 
         var movie2 = db.getMovieWithTitle("17 Again");
-        db.createTag("Comedy", movie2.get().getId());
-        db.createTag("Drama", movie2.get().getId());
-        db.createTag("Fantasy", movie2.get().getId());
+        db.createTag("Comedy", movie2.get().getId(), "David", "public");
+        db.createTag("Drama", movie2.get().getId(), "David", "public");
+        db.createTag("Fantasy", movie2.get().getId(), "David", "public");
 
         db.createRating("They should get divorced", "3", "3", "David", movie2.get().getId(), "public");
         db.createRating("The best friend was the best character", "7", "7", "David", movie2.get().getId(), "public");
@@ -35,9 +35,9 @@ public class PopulationData {
                 "A young viking befriends a cat-like dragon. He also gets a date.");
 
         var movie3 = db.getMovieWithTitle("How to Train Your Dragon");
-        db.createTag("Animation", movie3.get().getId());
-        db.createTag("Action", movie3.get().getId());
-        db.createTag("Adventure", movie3.get().getId());
+        db.createTag("Animation", movie3.get().getId(), "David", "public");
+        db.createTag("Action", movie3.get().getId(), "David", "public");
+        db.createTag("Adventure", movie3.get().getId(), "David", "public");
 
         db.createRating("Good animated movies", "6", "6", "David", movie3.get().getId(), "public");
         db.createRating("Cute pet", "7", "7", "David", movie3.get().getId(), "public");
@@ -47,45 +47,45 @@ public class PopulationData {
                 "A rat can cook and cooks for the son of a famous chef.");
 
         var movie4 = db.getMovieWithTitle("Ratatouille");
-        db.createTag("Animation", movie4.get().getId());
-        db.createTag("Adventure", movie4.get().getId());
-        db.createTag("Comedy", movie4.get().getId());
+        db.createTag("Animation", movie4.get().getId(), "David", "public");
+        db.createTag("Adventure", movie4.get().getId(), "David", "public");
+        db.createTag("Comedy", movie4.get().getId(), "David", "public");
 
         db.createRating("Strong rats", "6", "6", "David", movie4.get().getId(), "public");
-        db.createRating("Animal prowess", "7", "7", "David", movie4.get().getId(), "public");
-        db.createRating("Dirty rat", "5", "5", "David", movie4.get().getId(), "public");
+        db.createRating("rats everywhere", "7", "7", "David", movie4.get().getId(), "public");
+        db.createRating("Too many rats", "5", "5", "David", movie4.get().getId(), "public");
 
         db.createMovie("Lilo and Stitch", "Dean DeBlois, Chris Sanders", "2002", "1h. 25min.", "Chris Sanders, Dean DeBlois",
                 "A girl named Lilo ends up adapting an alien by accident and things go down.");
 
         var movie5 = db.getMovieWithTitle("Lilo and Stitch");
-        db.createTag("Animation", movie5.get().getId());
-        db.createTag("Adventure", movie5.get().getId());
-        db.createTag("Comedy", movie5.get().getId());
-        db.createTag("Family", movie5.get().getId());
+        db.createTag("Animation", movie5.get().getId(), "David", "public");
+        db.createTag("Adventure", movie5.get().getId(), "David", "public");
+        db.createTag("Comedy", movie5.get().getId(), "David", "public");
+        db.createTag("Family", movie5.get().getId(), "David", "public");
 
         db.createRating("Best Movies of all time", "10", "10", "David", movie5.get().getId(), "public");
         db.createRating("Best family movies", "6", "6", "David", movie5.get().getId(), "public");
-        db.createRating("Hottest animated character", "10", "10", "David", movie5.get().getId(), "public");
+        db.createRating("Paul Approved", "10", "10", "David", movie5.get().getId(), "public");
 
         db.createMovie("Atlantis: The Lost Empire", "Gary Trousdale, Kirk Wise", "2001", "1h. 35min.", "Tab Murphy, Kirk Wise, Gary Trousdale",
                 "A failure of a researcher who’s super dorky ends up going on an expedition to Atlantis, this is an attractive man. And he gets a hot girlfriend at the end of the movie.");
 
         var movie6 = db.getMovieWithTitle("Atlantis: The Lost Empire");
-        db.createTag("Animation", movie6.get().getId());
-        db.createTag("Action", movie6.get().getId());
-        db.createTag("Adventure", movie6.get().getId());
+        db.createTag("Animation", movie6.get().getId(), "David", "public");
+        db.createTag("Action", movie6.get().getId(), "David", "public");
+        db.createTag("Adventure", movie6.get().getId(), "David", "public");
 
         db.createRating("Animated movies with somber deaths", "10", "10", "David", movie6.get().getId(), "public");
-        db.createRating("Hottest animated character", "10", "10", "David", movie6.get().getId(), "public");
+        db.createRating("Paul Approved", "10", "10", "David", movie6.get().getId(), "public");
 
         db.createMovie("West Side Story", "Jerome Robbins, Robert Wise", "1961", "2h. 33min.", "Ernest Lehman, Arthur Laurents, Jerome Robbins",
                 "Lots of good-looking people fight like Romeo and Juliet in a modern gang thing. It’s a musical.");
 
         var movie7 = db.getMovieWithTitle("West Side Story");
-        db.createTag("Crime", movie7.get().getId());
-        db.createTag("Drama", movie7.get().getId());
-        db.createTag("Musical", movie7.get().getId());
+        db.createTag("Crime", movie7.get().getId(), "David", "public");
+        db.createTag("Drama", movie7.get().getId(), "David", "public");
+        db.createTag("Musical", movie7.get().getId(), "David", "public");
 
         db.createRating("Best musical", "10", "10", "David", movie7.get().getId(), "public");
         db.createRating("Feel good movies", "10", "10", "David", movie7.get().getId(), "public");
@@ -94,9 +94,9 @@ public class PopulationData {
                 "A chick who’s on a cheerleading team that steals routines from another way cooler school ends up being captain and changes her team’s ways. They end up losing to the way cooler team.");
 
         var movie8 = db.getMovieWithTitle("Bring It On");
-        db.createTag("Comedy", movie8.get().getId());
-        db.createTag("Romance", movie8.get().getId());
-        db.createTag("Sport", movie8.get().getId());
+        db.createTag("Comedy", movie8.get().getId(), "David", "public");
+        db.createTag("Romance", movie8.get().getId(), "David", "public");
+        db.createTag("Sport", movie8.get().getId(), "David", "public");
 
         db.createRating("Best Cheerleading movie", "6", "6", "David", movie8.get().getId(), "public");
         db.createRating("Cutest Outfits", "3", "3", "David", movie8.get().getId(), "public");
@@ -105,9 +105,9 @@ public class PopulationData {
                 "An animated fairy-tale chick ends up falling into a well and ends up getting with a man who’s engaged. She was also engaged and having her wedding day before she fell into the well. They end up having an emotional affair and get together at the end of the movie. It’s so cute and their partners get together and live way better lives than them.");
 
         var movie9 = db.getMovieWithTitle("Enchanted");
-        db.createTag("Animation", movie9.get().getId());
-        db.createTag("Adventure", movie9.get().getId());
-        db.createTag("Comedy", movie9.get().getId());
+        db.createTag("Animation", movie9.get().getId(), "David", "public");
+        db.createTag("Adventure", movie9.get().getId(), "David", "public");
+        db.createTag("Comedy", movie9.get().getId(), "David", "public");
 
         db.createRating("Best Movie of ALL TIME", "10", "10", "David", movie9.get().getId(), "public");
         db.createRating("Best side character of all time", "3", "3", "David", movie9.get().getId(), "public");
@@ -118,22 +118,22 @@ public class PopulationData {
                 "Two brother-like friends end up stranded on an island and deceive a bunch of natives into thinking they’re gods. They end up saving the island and leaving it though.");
 
         var movie10 = db.getMovieWithTitle("The Road to El Dorado");
-        db.createTag("Animation", movie10.get().getId());
-        db.createTag("Adventure", movie10.get().getId());
-        db.createTag("Comedy", movie10.get().getId());
+        db.createTag("Animation", movie10.get().getId(), "David", "public");
+        db.createTag("Adventure", movie10.get().getId(), "David", "public");
+        db.createTag("Comedy", movie10.get().getId(), "David", "public");
 
         db.createRating("BEST movie of all time", "10", "10", "David", movie10.get().getId(), "public");
         db.createRating("Best bros United", "8", "8", "David", movie10.get().getId(), "public");
         db.createRating("Summer-time movies", "9", "9", "David", movie10.get().getId(), "public");
-        db.createRating("Hottest animated character", "10", "10", "David", movie10.get().getId(), "public");
+        db.createRating("Paul Approved", "10", "10", "David", movie10.get().getId(), "public");
 
         db.createMovie("Barbie", "Greta Gerwig", "2023", "1h. 45min.", "Greta Gerwig, Noah Baumbach",
                 "Barbie becomes imperfect and wants to fix herself so she goes to the real world and then wants to stay imperfect. Ken is also a menace.");
 
         var movie11 = db.getMovieWithTitle("Barbie");
-        db.createTag("Adventure", movie11.get().getId());
-        db.createTag("Comedy", movie11.get().getId());
-        db.createTag("Fantasy", movie11.get().getId());
+        db.createTag("Adventure", movie11.get().getId(), "David", "public");
+        db.createTag("Comedy", movie11.get().getId(), "David", "public");
+        db.createTag("Fantasy", movie11.get().getId(), "David", "public");
 
         db.createRating("Camp movies", "10", "10", "David", movie11.get().getId(), "public");
         db.createRating("Existential Crises", "5", "5", "David", movie11.get().getId(), "public");
@@ -143,9 +143,9 @@ public class PopulationData {
                 "An adaption of the prince and the pauper but with Barbie.");
 
         var movie12 = db.getMovieWithTitle("Barbie as The Princess and the Pauper");
-        db.createTag("Animation", movie12.get().getId());
-        db.createTag("Comedy", movie12.get().getId());
-        db.createTag("Family", movie12.get().getId());
+        db.createTag("Animation", movie12.get().getId(), "David", "public");
+        db.createTag("Comedy", movie12.get().getId(), "David", "public");
+        db.createTag("Family", movie12.get().getId(), "David", "public");
 
         db.createRating("Best Barbie movie of all time", "10", "10", "David", movie12.get().getId(), "public");
         db.createRating("Best animated movie of all time", "5", "5", "David", movie12.get().getId(), "public");
@@ -155,9 +155,9 @@ public class PopulationData {
                 "A chick who can’t throw hands ends up meeting a prince and they live happily ever after.");
 
         var movie13 = db.getMovieWithTitle("Cinderella");
-        db.createTag("Family", movie13.get().getId());
-        db.createTag("Fantasy", movie13.get().getId());
-        db.createTag("Musical", movie13.get().getId());
+        db.createTag("Family", movie13.get().getId(), "David", "public");
+        db.createTag("Fantasy", movie13.get().getId(), "David", "public");
+        db.createTag("Musical", movie13.get().getId(), "David", "public");
 
         db.createRating("Live-Action Princess Movie", "4", "4", "David", movie13.get().getId(), "public");
         db.createRating("Confusing movies", "10", "10", "David", movie13.get().getId(), "public");
@@ -166,9 +166,9 @@ public class PopulationData {
                 "Young lion cub murders his father cause he can’t follow instructions. He then fights his uncle to take his father’s place as leader of the lion pack.");
 
         var movie14 = db.getMovieWithTitle("The Lion King");
-        db.createTag("Animation", movie14.get().getId());
-        db.createTag("Adventure", movie14.get().getId());
-        db.createTag("Drama", movie14.get().getId());
+        db.createTag("Animation", movie14.get().getId(), "David", "public");
+        db.createTag("Adventure", movie14.get().getId(), "David", "public");
+        db.createTag("Drama", movie14.get().getId(), "David", "public");
 
         db.createRating("Best animated movie of all time", "7", "7", "David", movie14.get().getId(), "public");
 
@@ -176,21 +176,21 @@ public class PopulationData {
                 "A girl who has life figured out gets bothered by a frog and her life becomes awful. But they fall in love and live happily ever after.");
 
         var movie15 = db.getMovieWithTitle("The Princess and the Frog");
-        db.createTag("Animation", movie15.get().getId());
-        db.createTag("Adventure", movie15.get().getId());
-        db.createTag("Comedy", movie15.get().getId());
+        db.createTag("Animation", movie15.get().getId(), "David", "public");
+        db.createTag("Adventure", movie15.get().getId(), "David", "public");
+        db.createTag("Comedy", movie15.get().getId(), "David", "public");
 
         db.createRating("Animated Disney Movie Goodness", "2", "2", "David", movie15.get().getId(), "public");
         db.createRating("Princess PrincessNess", "8", "8", "David", movie15.get().getId(), "public");
-        db.createRating("Pretty Clothes", "6", "6", "David", movie15.get().getId(), "public");
+        db.createRating("Aesthetic", "6", "6", "David", movie15.get().getId(), "public");
 
         db.createMovie("Tangled", "Nathan Greno, Byron Howard", "2010", "1h. 40min.", "Dan Fogelman, Jacob Grimm, Wilhelm Grimm",
                 "A girl leaves the tower that her mom doesn’t want her to and falls in love. She was also imprisoned in said tower for years. And stolen from her parents.");
 
         var movie16 = db.getMovieWithTitle("Tangled");
-        db.createTag("Animation", movie16.get().getId());
-        db.createTag("Adventure", movie16.get().getId());
-        db.createTag("Comedy", movie16.get().getId());
+        db.createTag("Animation", movie16.get().getId(), "David", "public");
+        db.createTag("Adventure", movie16.get().getId(), "David", "public");
+        db.createTag("Comedy", movie16.get().getId(), "David", "public");
 
         db.createRating("Disney Princess Movie", "10", "10", "David", movie16.get().getId(), "public");
         db.createRating("Quirky Disney Character rating", "7", "7", "David", movie16.get().getId(), "public");
@@ -200,9 +200,9 @@ public class PopulationData {
                 "A girl goes to war in place of her father and saves China. She also has a pet dragon and takes home a hot general.");
 
         var movie17 = db.getMovieWithTitle("Mulan");
-        db.createTag("Animation", movie17.get().getId());
-        db.createTag("Adventure", movie17.get().getId());
-        db.createTag("Comedy", movie17.get().getId());
+        db.createTag("Animation", movie17.get().getId(), "David", "public");
+        db.createTag("Adventure", movie17.get().getId(), "David", "public");
+        db.createTag("Comedy", movie17.get().getId(), "David", "public");
 
         db.createRating("Disney Princess Movie", "10", "10", "David", movie17.get().getId(), "public");
         db.createRating("Princess PrincessNess", "8", "8", "David", movie17.get().getId(), "public");
@@ -213,9 +213,9 @@ public class PopulationData {
                 "A boy becomes a superhero because he has to fight his father or something.");
 
         var movie18 = db.getMovieWithTitle("Shang-Chi and the Legend of the 10 Rings");
-        db.createTag("Action", movie18.get().getId());
-        db.createTag("Adventure", movie18.get().getId());
-        db.createTag("Comedy", movie18.get().getId());
+        db.createTag("Action", movie18.get().getId(), "David", "public");
+        db.createTag("Adventure", movie18.get().getId(), "David", "public");
+        db.createTag("Comedy", movie18.get().getId(), "David", "public");
 
         db.createRating("Superhero origins", "4", "4", "David", movie18.get().getId(), "public");
         db.createRating("Cutest male lead", "9", "9", "David", movie18.get().getId(), "public");
@@ -225,9 +225,9 @@ public class PopulationData {
                 "A middle-aged Chinese immigrant goes on an adventure exploring other universes.");
 
         var movie19 = db.getMovieWithTitle("Everything Everywhere All at Once");
-        db.createTag("Action", movie19.get().getId());
-        db.createTag("Adventure", movie19.get().getId());
-        db.createTag("Comedy", movie19.get().getId());
+        db.createTag("Action", movie19.get().getId(), "David", "public");
+        db.createTag("Adventure", movie19.get().getId(), "David", "public");
+        db.createTag("Comedy", movie19.get().getId(), "David", "public");
 
         db.createRating("Costume rating", "7", "7", "David", movie19.get().getId(), "public");
         db.createRating("Weirdest people", "4", "4", "David", movie19.get().getId(), "public");
@@ -237,9 +237,9 @@ public class PopulationData {
                 "Young Spider Boy has another spider-verse adventure.");
 
         var movie20 = db.getMovieWithTitle("Spider-Man: Across the Spider-Verse");
-        db.createTag("Animation", movie20.get().getId());
-        db.createTag("Action", movie20.get().getId());
-        db.createTag("Adventure", movie20.get().getId());
+        db.createTag("Animation", movie20.get().getId(), "David", "public");
+        db.createTag("Action", movie20.get().getId(), "David", "public");
+        db.createTag("Adventure", movie20.get().getId(), "David", "public");
 
         db.createRating("Best animated movie", "10", "10", "David", movie20.get().getId(), "public");
         db.createRating("Movie of the year", "9", "9", "David", movie20.get().getId(), "public");
