bf578e1931f08189ef244fdb486a9ec0a6fdf67d
DeveloperDave17 <DavidHennigan17@gmail.com>
2023-10-31 09:59:57 -0400

    add movie data to startup

--- buildAutomation/startReview.sh
+++ buildAutomation/startReview.sh
@@ -10,6 +10,9 @@ docker container remove reel-rating-review-data-service
 docker container stop reel-rating-mongo-movie
 docker container remove reel-rating-mongo-movie
 
+docker container stop reel-rating-movie-data-service
+docker container remove reel-rating-movie-data-service
+
 cd ..
 
 docker compose -f "docker-compose-review.yaml" up -d --build
