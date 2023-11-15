660014dc13597a6c27867a36350de51a5fc86852
DeveloperDave17 <DavidHennigan17@gmail.com>
2023-11-15 14:06:09 -0500

    fix mongodb connection issue

--- backend/reel-rating-rating-data-service/src/main/java/edu/oswego/cs/rest/DatabaseController.java
+++ backend/reel-rating-rating-data-service/src/main/java/edu/oswego/cs/rest/DatabaseController.java
@@ -18,14 +18,14 @@ import java.util.Arrays;
 import java.util.List;
 
 public class DatabaseController {
-  String mongoDatabaseName = System.getenv("MONGO_MOVIE_DATABASE_NAME");
-  String mongoURL = System.getenv("MONGO_MOVIE_URL");
+  private static String mongoDatabaseName = System.getenv("MONGO_MOVIE_DATABASE_NAME");
+  private static String mongoURL = System.getenv("MONGO_MOVIE_URL");
+  private static MongoClient mongoClient = MongoClients.create(mongoURL);
 
   /*
    *
    */
   public MongoDatabase getMovieDatabase() {
-    MongoClient mongoClient = MongoClients.create(mongoURL);
     return mongoClient.getDatabase(mongoDatabaseName);
   }
 
